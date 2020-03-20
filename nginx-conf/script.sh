e164c2a2c11113790cdb7dc3a93197efc2b0bd52
Oprational Vascomm <ardaputrakusuma07@gmail.com>
2020-03-20 15:02:14 +0700

    Update script.sh

--- nginx-conf/script.sh
+++ nginx-conf/script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 chmod 777 -R /var/www/html 
-php artisan key:generate
+php artisan key:generate --force
 php artisan cache:clear
 php artisan config:clear
 #php artisan storage:link
