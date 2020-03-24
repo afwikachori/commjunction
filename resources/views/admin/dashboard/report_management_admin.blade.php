5e7d797f70fbe3901000902be7a85969a70d89e3
AFWIKA CHORI <afwikachori@gmail.com>
2020-03-24 18:50:53 +0700

    report management css done

--- resources/views/admin/dashboard/report_management_admin.blade.php
+++ resources/views/admin/dashboard/report_management_admin.blade.php
@@ -103,70 +103,7 @@
                                     </tr>
                                 </thead>
                             </table>
-                            <hr>
-<div class="accordion" id="accordion_reportsubs">
-    <div class="card">
-        <div class="card-header" id="headingOne">
-            <h2 class="mb-0">
-                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
-                    aria-expanded="true" aria-controls="collapseOne">
-                    Collapsible Group Item #1
-                </button>
-            </h2>
-        </div>
 
-        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion_reportsubs">
-            <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-                aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-header" id="headingTwo">
-            <h2 class="mb-0">
-                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="false" aria-controls="collapseTwo">
-                    Collapsible Group Item #2
-                </button>
-            </h2>
-        </div>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion_reportsubs">
-            <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-                aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-header" id="headingThree">
-            <h2 class="mb-0">
-                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree"
-                    aria-expanded="false" aria-controls="collapseThree">
-                    Collapsible Group Item #3
-                </button>
-            </h2>
-        </div>
-        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion_reportsubs">
-            <div class="card-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf
-                moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
-                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-                shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea
-                proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-                aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
-        </div>
-    </div>
-</div>
                         </div>
 
                     </div>
@@ -776,15 +713,43 @@
 
 
 
-                        var isshow ='';
+                        var isshow = '';
+                        var isshow2 = '';
                         $.each(result, function (i, isi) {
                             var tgl = dateFormatReport(isi.date);
-                           isshow += '<li>' + isi.module + '&nbsp; <span class="cgrey">|</span> &nbsp; <small class="cteal2">' + tgl + '</small>' +
+                            isshow2 += '<li>' + isi.module + '&nbsp; <span class="cgrey">|</span> &nbsp; <small class="cteal2">' + tgl + '</small>' +
                                 '<br>' +
                                 '<label class="cgrey2">' + isi.endpoint + '</label>' +
                                 '</li>';
+
+                            isshow += '<div class="card"> ' +
+                                '<div class="card-header" id="heading' + i+ '" >' +
+                                '<h2 class="mb-0">' +
+                                '<button class="btn btn-link" type="button" data-toggle="collapse"' +
+                                'data-target="#collapse'+i+'" aria-expanded="true"' +
+                                'aria-controls="collapse'+ i+'">' +
+                                '<i class="mdi mdi-chevron-down"></i> &nbsp;' +
+                                '<span class="cgrey s16">' + isi.module +'</span> &nbsp;<span' +
+                                    'class="cteal">' + tgl + '</span>' +
+                                    '</button>' +
+                                    '</h2>' +
+                                    '</div >' +
+                                    '<div id="collapse'+i+'" class="collapse" aria-labelledby="heading' + i + '"' +
+                                    'data-parent="#accordion_reportsubs">' +
+                                    '<div class="card-body s14 clight">' +
+                                    '<div class="row" style="margin-left: 0.5em;">' +
+                                    '<div class="col-12">' +
+                                    'ID : <div class="mb-1 cgrey2"> ' + isi._id + '</div>' +
+                                    'Date : <div class="mb-1 cgrey2">' + tgl + '</div>' +
+                                    'Endpoint : <div class="mb-1 cgrey2">' + isi.endpoint + '</div>' +
+                                    '</div>' +
+                                    '</div>' +
+                                    '</div>' +
+                                    '</div>' +
+                                    '</div > ';
+
                         });
-                        var layot = "<div class='text-wrap width-300'><ul>" + isshow + "</ul></div>";
+                        var layot = '<div class="text-wrap width-500"><div class="accordion" id="accordion_reportsubs">'+isshow+'</div></div>';
                         return layot;
                     }
                 },
@@ -795,16 +760,16 @@
     }
 
     function dateFormatReport(tgl) {
-            var d = new Date(tgl);
+        var d = new Date(tgl);
 
-            dformat = [d.getDate(), d.getMonth() + 1,
-            d.getFullYear()].join('/') + ' &nbsp;   &nbsp;  ' +
-                [d.getHours(),
-                d.getMinutes(),
-                d.getSeconds()].join(':');
+        dformat = [d.getDate(), d.getMonth() + 1,
+        d.getFullYear()].join('/') + ' &nbsp;  ' +
+            [d.getHours(),
+            d.getMinutes(),
+            d.getSeconds()].join(':');
 
-            return dformat;
-        }
+        return dformat;
+    }
 
 </script>
 
