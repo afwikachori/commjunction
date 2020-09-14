e93b885b45f3bf8ab1232b9552c9bab79ffd035d
Mochammad Machfudh <cmmachfudh@gmail.com>
2020-09-14 14:19:22 +0700

    normalisasi / Grouping menu admin company

--- resources/views/admin/dashboard/admin_sidebar.blade.php
+++ resources/views/admin/dashboard/admin_sidebar.blade.php
@@ -15,83 +15,77 @@
             <i class="mdi mdi-home menu-icon"></i>
         </a>
     </li>
+
     <li class="nav-item">
-        <a class="nav-link" href="/admin/subs_management">
-            <span class="menu-title" lang="en">Subscriber Management</span>
+        <a class="nav-link" data-toggle="collapse" href="#general-pages" aria-expanded="false"
+            aria-controls="general-pages">
+            <span class="menu-title">Member Management</span>
+            <i class="mdi mdi-arrow-down-drop-circle menu-arrow"></i>
             <i class="mdi mdi-account-multiple menu-icon"></i>
         </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="/admin/membership_management">
-            <span class="menu-title" lang="en">Membership Management</span>
-            <i class="mdi mdi-account-card-details menu-icon"></i>
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="/admin/user_management">
-            <span class="menu-title" lang="en">User Management</span>
-            <i class="mdi mdi-account-plus menu-icon"></i>
-        </a>
+        <div class="collapse" id="general-pages">
+            <ul class="nav sub-sidebar flex-column sub-menu">
+                <li class="nav-item sub-sidebar">
+                    <a class="nav-link" href="/admin/subs_management">Subscriber Management</a>
+                </li>
+                <li class="nav-item sub-sidebar">
+                    <a class="nav-link" href="/admin/membership_management">Membership Management</a>
+                </li>
+            </ul>
+        </div>
     </li>
 
-
     <li class="nav-item">
-        <a class="nav-link" href="/admin/module_management">
-            <span class="menu-title" lang="en">Module Management</span>
+        <a class="nav-link" data-toggle="collapse" href="#modules" aria-expanded="false"
+            aria-controls="modules">
+            <span class="menu-title">Modules</span>
+            <i class="mdi mdi-arrow-down-drop-circle menu-arrow"></i>
             <i class="mdi mdi-view-module menu-icon"></i>
         </a>
-    </li>
-
-
-    <li class="nav-item">
-        <a class="nav-link" href="/admin/usertype_management">
-            <span class="menu-title" lang="en">User Type Management</span>
-            <i class="mdi mdi-account-convert menu-icon"></i>
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="/admin/transaction">
-            <span class="menu-title" lang="en">Transaction Management</span>
-            <i class="mdi mdi-cart menu-icon"></i>
-        </a>
+        <div class="collapse" id="modules">
+            <ul class="nav sub-sidebar flex-column sub-menu">
+                <li class="nav-item sub-sidebar">
+                    <a class="nav-link" href="/admin/news_management">News Management</a>
+                </li>
+            </ul>
+        </div>
     </li>
 
     <li class="nav-item">
-        <a class="nav-link" href="/admin/report_management">
-            <span class="menu-title" lang="en">Report Management</span>
+        <a class="nav-link" data-toggle="collapse" href="#administration" aria-expanded="false"
+            aria-controls="administration">
+            <span class="menu-title">Administration</span>
+            <i class="mdi mdi-arrow-down-drop-circle menu-arrow"></i>
             <i class="mdi mdi-buffer menu-icon"></i>
         </a>
-    </li>
-
-
-    <li class="nav-item">
-        <a class="nav-link" href="/admin/payment_management">
-            <span class="menu-title" lang="en">Payment Management</span>
-            <i class="mdi mdi-credit-card menu-icon"></i>
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="/admin/notification_management">
-            <span class="menu-title" lang="en">Notification Management</span>
-            <i class="mdi mdi-email-outline menu-icon"></i>
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="/admin/inbox_management">
-            <span class="menu-title" lang="en">Inbox Management</span>
-            <i class="mdi mdi-email menu-icon"></i>
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="/admin/news_management">
-            <span class="menu-title" lang="en">News Management</span>
-            <i class="mdi mdi-message-text-outline menu-icon"></i>
-        </a>
+        <div class="collapse" id="administration">
+            <ul class="nav sub-sidebar flex-column sub-menu">
+                <li class="nav-item sub-sidebar">
+                    <a class="nav-link" href="/admin/user_management">User Management</a>
+                </li>
+                <li class="nav-item sub-sidebar">
+                    <a class="nav-link" href="/admin/usertype_management">User Type Management</a>
+                </li>
+                <li class="nav-item sub-sidebar">
+                    <a class="nav-link" href="/admin/module_management">Module Management</a>
+                </li>
+                <li class="nav-item sub-sidebar">
+                    <a class="nav-link" href="/admin/report_management">Report Management</a>
+                </li>
+                <li class="nav-item sub-sidebar">
+                    <a class="nav-link" href="/admin/transaction">Transaction Management</a>
+                </li>
+                <li class="nav-item sub-sidebar">
+                    <a class="nav-link" href="/admin/payment_management">Payment Management</a>
+                </li>
+                <li class="nav-item sub-sidebar">
+                    <a class="nav-link" href="/admin/notification_management">Notification Management</a>
+                </li>
+                <li class="nav-item sub-sidebar">
+                    <a class="nav-link" href="/admin/inbox_management">Inbox Management</a>
+                </li>
+            </ul>
+        </div>
     </li>
 
         {{-- <li class="nav-item">
